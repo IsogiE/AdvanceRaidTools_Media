cd7474dd7f10c960da5ae547586949a661a55625
IsogiE <88090493+IsogiE@users.noreply.github.com>
2026-04-29 16:29:32 +0200

    Media

---
+++ Dreams.lua
@@ -0,0 +1,36 @@
+local LSM = _G.LibStub and LibStub("LibSharedMedia-3.0", true)
+if not LSM then
+    return
+end
+
+local PACK = "ART"
+local BASE = [[Interface\AddOns\AdvanceRaidTools_Media\Images\Dreams\]]
+local PREFIX = "ART_Dream_" .. PACK .. "_"
+
+local DREAMS = {
+    { "oatly", "oatly.png", 256, 384 },
+    { "chill", "chillguy.png", 168, 300 },
+    { "lazel", "lazel.png", 960, 540 },
+    { "scrapps", "scrapps.png", 392, 345 },
+    { "vikrally", "vikrally.png", 540, 435 },
+    { "soul", "soul.png", 453, 435 },
+    { "shu", "shu.png", 540, 305 },
+    { "whytho", "whytho.png", 360, 287 },
+    { "lazelloot", "lazelloot.png", 358, 249 },
+    { "sjele", "sjele.png", 353, 418 },
+    { "andy", "andy.png", 540, 380 },
+    { "newpatch", "newpatch.png", 540, 360 },
+    { "norkus", "norkus.png", 379, 388 },
+    { "soulflat", "soulflat.png", 499, 297 },
+    { "speed", "speed.png", 381, 435 },
+    { "gulp", "gulp.png", 487, 471 },
+}
+
+ART_Media = ART_Media or {}
+ART_Media.Dreams = ART_Media.Dreams or { prefix = "ART_Dream_", dims = {} }
+
+for _, d in ipairs(DREAMS) do
+    local lsmName = PREFIX .. d[1]
+    LSM:Register(LSM.MediaType.BACKGROUND, lsmName, BASE .. d[2])
+    ART_Media.Dreams.dims[lsmName] = { w = d[3], h = d[4] }
+end
